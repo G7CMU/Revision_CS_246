bed8c03a40bf2672042c2f5b08dae7d1e2b4cb56
realkhoa <khoa.nguyenba@outlook.com>
2024-04-02 10:57:43 +0700

    run each class method

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,14 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.print("Hello World");
+		System.out.println("10 to binary: ");
+		BaseN.toBinary(10);
+		System.out.println("Circle Area: ");
+		ShapeCalculator.circleArea(12);
+		System.out.println("1 + 2: ");
+		Calc.sum(1, 2);
+		System.out.println("3x^2 + 3x - 2 = 0: ");
+		EquationSolver.quadraticEquation(3, 4, -2);
 	}
 
 }
