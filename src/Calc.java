2f7b37ea194b5691abc79998f03254fd4b82b7cd
Ho Thanh Tien <hothanhtienqb@gmail.com>
2024-04-02 09:46:01 +0700

    commit2

--- src/Calc.java
+++ src/Calc.java
@@ -1,4 +1,18 @@
 
 public class Calc {
+	public static void sum(double a, double b) {
+		System.out.println("Sum = " + (a + b));
+	}
 
+	public static void divide(double a, double b) {
+		System.out.println("Sum = " + (a / b));
+	}
+
+	public static void multiply(double a, double b) {
+		System.out.println("Sum = " + (a * b));
+	}
+
+	public static void subtract(double a, double b) {
+		System.out.println("Sum = " + (a - b));
+	}
 }
