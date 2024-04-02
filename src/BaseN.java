c55b2c44868ec610fe7bcbec51ef93330924100f
hotanphong <phongho763@gmail.com>
2024-04-02 10:16:37 +0700

    add base number

---
+++ src/BaseN.java
@@ -0,0 +1,9 @@
+public class BaseN {
+	public static void toBinary(int n) {
+		System.out.println("Binary = " + Integer.toBinaryString(n));
+	}
+	
+	public static void toHex(int n) {
+		System.out.println("Binary = " + Integer.toHexString(n));
+	}
+}
